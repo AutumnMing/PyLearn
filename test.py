30aea56b2007aa21bfe46ec57b430620a9cd7050
DaGongRen <35247025+AutumnMing@users.noreply.github.com>
2022-09-30 09:06:13 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+import os
+import PySide6.QtCore
+
+os.getcwd()
+# 测试文件
+a: list[int] = [1, 2, 3, 4, 5]
+
+
+b: list[int] = [1, 2, 3]
+
+
+dict_a: dict[str:int] = {'a':1, 'b':2, 'c':3}
+print('原始字典a', dict_a, sep=' : ')
+dict_b: dict[str:int] = {'a':100}
+_ = dict_a | dict_b
+print('更新后的字典a', dict_a, sep=' : ')
+
